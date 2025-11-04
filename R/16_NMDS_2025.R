f341464e7a053e0c0271fe58a26c634a7e194e9d
hannahmarkant <hannah.markant@posteo.de>
2025-11-04 17:05:10 +0100

    Update 16_NMDS_2025.R

--- R/16_NMDS_2025.R
+++ R/16_NMDS_2025.R
@@ -16,7 +16,6 @@ library(labdsv)
 library(vegan)
 library(ggplot2)
 library(ggrepel)
-library(indicspecies)
 library(grid)
 library(lubridate)
 
@@ -277,4 +276,5 @@ NMDS_plot_combined <- ggplot(nmds.sites, aes(NMDS1, NMDS2)) +
     axis.title = element_text(size = 11),   
     axis.text = element_text(size = 10))
 print(NMDS_plot_combined)
+
 
